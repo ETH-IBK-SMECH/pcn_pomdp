fee9ac65fffd0cea2f5e336370bde38bf6bf1c01
Kiran Bacsa <kbacsa-ethz>
2024-05-06 16:22:32 +0800

    add multiple agents that collect data in parralel

--- data_simulation.py
+++ data_simulation.py
@@ -42,7 +42,10 @@ def mdp_sequences(loop_graph, p_transition, mus, sigmas, num_iterations):
 
     n_nodes = len(loop_graph.nodes)
     n_states = p_transition.shape[1]
-    agent = Agent(loop_graph)
+
+    n_agents = 3
+
+    agents = [Agent(loop_graph) for _ in range(n_agents)]
 
     # Start simulation
     all_emissions = np.full((n_nodes, num_iterations), np.nan)
@@ -53,31 +56,32 @@ def mdp_sequences(loop_graph, p_transition, mus, sigmas, num_iterations):
     states = np.zeros(n_nodes).astype(int)
 
     for iteration in tqdm(np.arange(num_iterations)):
-
-        # location information
-        current_state = states[agent.loop_path[agent.current_idx]]
-
-        # emission (Homoscedastic gaussian)
-        emission = norm.rvs(loc=mus[current_state], scale=sigmas[current_state])
-        emission = np.clip(int(emission), 0, n_states - 1)
-
-        # update chain
-        all_emissions[agent.loop_path[agent.current_idx], iteration] = emission
-
         # transition update
         for i in range(n_nodes):
-            action = 0
-            if agent.loop_path[agent.current_idx] == i:
-                if emission == (n_states - 1): # if the worst state is detected, repair
-                    action = 1
-            all_actions[i, iteration] = action
-            states[i] = markov_update(p_transition[action], states[i])
-        all_states[:, iteration] = states
+            states[i] = markov_update(p_transition[0], states[i])
 
-        # get new position info
-        agent.current_idx += 1
-        if agent.loop_path[agent.current_idx] == agent.last_node:
-            agent.recalculate_path()
+        for agent in agents:
+            # location information
+            current_state = states[agent.loop_path[agent.current_idx]]
+
+            # emission (Homoscedastic gaussian)
+            emission = norm.rvs(loc=mus[current_state], scale=sigmas[current_state])
+            emission = np.clip(int(emission), 0, n_states - 1)
+
+            # update chain
+            all_emissions[agent.loop_path[agent.current_idx], iteration] = emission
+
+            if emission == (n_states - 1):  # if the worst state is detected, repair
+                action = 1
+                all_actions[agent.loop_path[agent.current_idx], iteration] = action
+                states[agent.loop_path[agent.current_idx]] = markov_update(p_transition[action], states[agent.loop_path[agent.current_idx]])
+
+            # get new position info
+            agent.current_idx += 1
+            if agent.loop_path[agent.current_idx] == agent.last_node:
+                agent.recalculate_path()
+
+        all_states[:, iteration] = states
 
     return {
         'states': all_states,
